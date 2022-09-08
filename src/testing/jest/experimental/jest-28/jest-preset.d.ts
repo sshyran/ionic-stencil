c3747a3acbb827658df8250fb73bd49f63cb618f
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-09-08 13:31:13 -0400

    add .d.ts for now

---
+++ src/testing/jest/experimental/jest-28/jest-preset.d.ts
@@ -0,0 +1,9 @@
+/**
+ * The path's declared below are relative. Specifically, they are relative to the location of this file after
+ * compilation of the Stencil compiler has completed. See `scripts/bundles/testing` for the location of this file
+ * following compilation.
+ */
+declare const path: any;
+declare const testingDir: string;
+declare const moduleExtensions: string[];
+declare const moduleExtensionRegexp: string;
