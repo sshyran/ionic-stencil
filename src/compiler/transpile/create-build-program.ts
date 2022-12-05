ionic-team/stencil

fix(compiler): destroy callback naming (#3289)
Fix Typos for `addDestroy` (`addDestory`) and `removeDestroy` (`removeDestory`)

BREAKING CHANGE: Public APIs `addDestroy` (`addDestory`) and `removeDestroy`
(`removeDestory`) have been renamed to fix typos

src/compiler/transpile/create-build-program.ts
@@ -43,7 +43,7 @@ export const createTsBuildProgram = async (
     },
   };
 
-  config.sys.addDestory(() => tsWatchSys.clearTimeout(timeoutId));
+  config.sys.addDestroy(() => tsWatchSys.clearTimeout(timeoutId));
 
   const tsWatchHost = ts.createWatchCompilerHost(
     config.tsconfig,

sshyran/ionic-stencil

src/compiler/transpile/create-build-program.ts
@@ -71,7 +71,7 @@ export const createTsBuildProgram = async (
     },
   };
 
-  config.sys.addDestory(() => tsWatchSys.clearTimeout(currentBuildTimeoutId));
+  config.sys.addDestroy(() => tsWatchSys.clearTimeout(currentBuildTimeoutId));
 
   /**
    * Create a {@link ts.WatchCompilerHost}. A CompilerHost allows a {@link ts.Program} to interact with the