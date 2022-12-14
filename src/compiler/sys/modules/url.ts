b3d1099a913415c524152e078317724ca50870ac
Alice Pote <alice.writes.wrongs@gmail.com>
2022-12-14 13:58:44 -0500

    port the URL fix

--- src/compiler/sys/modules/url.ts
+++ src/compiler/sys/modules/url.ts
@@ -1,1 +1,7 @@
-export const URL = globalThis.URL as any;
+import { basicURLParse, serializePath, serializeURL } from 'whatwg-url';
+    // inlineDynamicImports: true,
+export { URL } from 'whatwg-url';
+
+export const pathToFileURL = (path: string) => serializeURL(basicURLParse(path, { stateOverride: 'file' }));
+
+export const fileURLToPath = (fileURL: string) => serializePath(basicURLParse(fileURL));
