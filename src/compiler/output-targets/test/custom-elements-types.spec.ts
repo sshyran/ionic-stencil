ionic-team/stencil

feat(compiler): add `CustomElementExportBehavior` to custom elements … (#3562)
This commit adds a config option to the `dist-custom-elements` output target that will control the behavior for re-exporting class definitions and (eventually) defining them as custom elements. Essentially, when this option is not set, this output target will revert to the previous export behavior from v2.16. Start of addressing STENCIL-500

src/compiler/output-targets/test/custom-elements-types.spec.ts
@@ -39,7 +39,7 @@ const setup = () => {
 };
 
 describe('Custom Elements Typedef generation', () => {
-  it('should generate an index.d.ts file corresponding to the index.js file', async () => {
+  it('should generate an index.d.ts file corresponding to the index.js file when barrel export behavior is enabled', async () => {
     // this component tests the 'happy path' of a component's filename coinciding with its
     // tag name
     const componentOne = stubComponentCompilerMeta({
@@ -54,6 +54,7 @@ describe('Custom Elements Typedef generation', () => {
       tagName: 'my-best-component',
     });
     const { config, compilerCtx, buildCtx } = setup();
+    (config.outputTargets[0] as d.OutputTargetDistCustomElements).customElementsExportBehavior = 'single-export-module';
     buildCtx.components = [componentOne, componentTwo];
 
     const writeFileSpy = jest.spyOn(compilerCtx.fs, 'writeFile');
@@ -90,6 +91,56 @@ describe('Custom Elements Typedef generation', () => {
       '}',
       'export declare const setPlatformOptions: (opts: SetPlatformOptions) => void;',
       "export * from '../types_dir/components';",
+      '',
+    ].join('\n');
+
+    expect(compilerCtx.fs.writeFile).toBeCalledWith(join('my-best-dir', 'index.d.ts'), expectedTypedefOutput, {
+      outputTargetType: DIST_CUSTOM_ELEMENTS,
+    });
+
+    writeFileSpy.mockRestore();
+  });
+
+  it('should generate an index.d.ts file corresponding to the index.js file when barrel export behavior is disabled', async () => {
+    // this component tests the 'happy path' of a component's filename coinciding with its
+    // tag name
+    const componentOne = stubComponentCompilerMeta({
+      tagName: 'my-component',
+      sourceFilePath: '/src/components/my-component/my-component.tsx',
+    });
+    // this component tests that we correctly resolve its path when the component tag does
+    // not match its filename
+    const componentTwo = stubComponentCompilerMeta({
+      sourceFilePath: '/src/components/the-other-component/my-real-best-component.tsx',
+      componentClassName: 'MyBestComponent',
+      tagName: 'my-best-component',
+    });
+    const { config, compilerCtx, buildCtx } = setup();
+    buildCtx.components = [componentOne, componentTwo];
+
+    const writeFileSpy = jest.spyOn(compilerCtx.fs, 'writeFile');
+
+    await generateCustomElementsTypes(config, compilerCtx, buildCtx, 'types_dir');
+
+    const expectedTypedefOutput = [
+      '/**',
+      ' * Used to manually set the base path where assets can be found.',
+      ' * If the script is used as "module", it\'s recommended to use "import.meta.url",',
+      ' * such as "setAssetPath(import.meta.url)". Other options include',
+      ' * "setAssetPath(document.currentScript.src)", or using a bundler\'s replace plugin to',
+      ' * dynamically set the path at build time, such as "setAssetPath(process.env.ASSET_PATH)".',
+      ' * But do note that this configuration depends on how your script is bundled, or lack of',
+      ' * bundling, and where your assets can be loaded from. Additionally custom bundling',
+      ' * will have to ensure the static assets are copied to its build directory.',
+      ' */',
+      'export declare const setAssetPath: (path: string) => void;',
+      '',
+      'export interface SetPlatformOptions {',
+      '  raf?: (c: FrameRequestCallback) => number;',
+      '  ael?: (el: EventTarget, eventName: string, listener: EventListenerOrEventListenerObject, options: boolean | AddEventListenerOptions) => void;',
+      '  rel?: (el: EventTarget, eventName: string, listener: EventListenerOrEventListenerObject, options: boolean | AddEventListenerOptions) => void;',
+      '}',
+      'export declare const setPlatformOptions: (opts: SetPlatformOptions) => void;',
       '',
     ].join('\n');
 

sshyran/ionic-stencil

src/compiler/output-targets/test/custom-elements-types.spec.ts
@@ -40,7 +40,7 @@ const setup = () => {
 };
 
 describe('Custom Elements Typedef generation', () => {
-  it('should generate an index.d.ts file corresponding to the index.js file', async () => {
+  it('should generate an index.d.ts file corresponding to the index.js file when barrel export behavior is enabled', async () => {
     // this component tests the 'happy path' of a component's filename coinciding with its
     // tag name
     const componentOne = stubComponentCompilerMeta({
@@ -55,6 +55,7 @@ describe('Custom Elements Typedef generation', () => {
       tagName: 'my-best-component',
     });
     const { config, compilerCtx, buildCtx } = setup();
+    (config.outputTargets[0] as d.OutputTargetDistCustomElements).customElementsExportBehavior = 'single-export-module';
     buildCtx.components = [componentOne, componentTwo];
 
     const writeFileSpy = jest.spyOn(compilerCtx.fs, 'writeFile');
@@ -100,6 +101,56 @@ describe('Custom Elements Typedef generation', () => {
       '}',
       'export declare const setPlatformOptions: (opts: SetPlatformOptions) => void;',
       "export * from '../types_dir/components';",
+      '',
+    ].join('\n');
+
+    expect(compilerCtx.fs.writeFile).toHaveBeenCalledWith(join('my-best-dir', 'index.d.ts'), expectedTypedefOutput, {
+      outputTargetType: DIST_CUSTOM_ELEMENTS,
+    });
+
+    writeFileSpy.mockRestore();
+  });
+
+  it('should generate an index.d.ts file corresponding to the index.js file when barrel export behavior is disabled', async () => {
+    // this component tests the 'happy path' of a component's filename coinciding with its
+    // tag name
+    const componentOne = stubComponentCompilerMeta({
+      tagName: 'my-component',
+      sourceFilePath: '/src/components/my-component/my-component.tsx',
+    });
+    // this component tests that we correctly resolve its path when the component tag does
+    // not match its filename
+    const componentTwo = stubComponentCompilerMeta({
+      sourceFilePath: '/src/components/the-other-component/my-real-best-component.tsx',
+      componentClassName: 'MyBestComponent',
+      tagName: 'my-best-component',
+    });
+    const { config, compilerCtx, buildCtx } = setup();
+    buildCtx.components = [componentOne, componentTwo];
+
+    const writeFileSpy = jest.spyOn(compilerCtx.fs, 'writeFile');
+
+    await generateCustomElementsTypes(config, compilerCtx, buildCtx, 'types_dir');
+
+    const expectedTypedefOutput = [
+      '/**',
+      ' * Used to manually set the base path where assets can be found.',
+      ' * If the script is used as "module", it\'s recommended to use "import.meta.url",',
+      ' * such as "setAssetPath(import.meta.url)". Other options include',
+      ' * "setAssetPath(document.currentScript.src)", or using a bundler\'s replace plugin to',
+      ' * dynamically set the path at build time, such as "setAssetPath(process.env.ASSET_PATH)".',
+      ' * But do note that this configuration depends on how your script is bundled, or lack of',
+      ' * bundling, and where your assets can be loaded from. Additionally custom bundling',
+      ' * will have to ensure the static assets are copied to its build directory.',
+      ' */',
+      'export declare const setAssetPath: (path: string) => void;',
+      '',
+      'export interface SetPlatformOptions {',
+      '  raf?: (c: FrameRequestCallback) => number;',
+      '  ael?: (el: EventTarget, eventName: string, listener: EventListenerOrEventListenerObject, options: boolean | AddEventListenerOptions) => void;',
+      '  rel?: (el: EventTarget, eventName: string, listener: EventListenerOrEventListenerObject, options: boolean | AddEventListenerOptions) => void;',
+      '}',
+      'export declare const setPlatformOptions: (opts: SetPlatformOptions) => void;',
       '',
     ].join('\n');
 